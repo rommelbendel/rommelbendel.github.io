4c156f20902ab8c06abad198a2590c01d5b1c8a9
Justus <47227577+Jonny2019@users.noreply.github.com>
2022-01-22 10:13:32 +0100

    added example

---
+++ app-challenge/JavaScript/style.js
@@ -0,0 +1,33 @@
+$(document).ready(function() {
+  const screenWidth = window.innerWidth;
+  const screenHeight = window.innerHeight;
+
+  var navBar = $('.NavBar');
+  var navBarLogo = $('#NavBarLogo');
+  var contentTransition = $('.ContentTransition');
+
+  if (screenWidth >= screenHeight) {
+    const logoscale = screenWidth / 18 + 'px';
+    navBarLogo.css({
+      width: logoscale,
+      height: logoscale
+    });
+  } else {
+    const logoscale = screenWidth / 6 + 'px';
+    navBarLogo.css({
+      width: logoscale,
+      height: logoscale
+    });
+  }
+
+  const transitionMargin = navBar.height();
+  const transitionMarginTop = transitionMargin + 'px';
+  const transitionHeight = screenHeight - transitionMargin + 'px';
+  const xShift = -1 * contentTransition.position()['left'] + 'px';
+  contentTransition.css({
+    marginTop: transitionMarginTop,
+    width: screenWidth,
+    height: transitionHeight,
+    marginLeft: xShift
+  });
+})
---
+++ app-challenge/index.php
@@ -0,0 +1,81 @@
+<!DOCTYPE html>
+<html lang="de" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Hypnochi</title>
+    <link rel="shortcut icon" href="./images/favicon.ico">
+    <link rel="stylesheet" href="./css/main.css">
+    <script src="https://code.jquery.com/jquery-latest.min.js" type="text/javascript"></script>
+    <script src="./JavaScript/style.js" charset="utf-8"></script>
+  </head>
+  <body>
+    <div class="NavBar">
+      <div class="NavBarLeft">
+        <ul>
+          <li>
+            <img src="./images/logo.png" alt="Logo" id="NavBarLogo">
+          </li>
+          <li>
+            <p id="PageTitle">Hypnochi</p>
+          </li>
+        </ul>
+      </div>
+      <div class="NavBarRight">
+        <ul>
+          <li>
+            <a href="#App">Hypnochi App</a>
+          </li>
+          <li>
+            <a href="#Handout">Handout</a>
+          </li>
+        </ul>
+      </div>
+    </div>
+    <div class="ContentTransition">
+      <div class="InfoBox">
+        <cite>
+          „Hypnose ist eine soziale Interaktion, in der eine Person einer anderen suggeriert,
+          dass bestimmte Wahrnehmungen, Gefühle, Gedanken oder Verhaltensweisen spontan auftreten." - Myers, David G.
+        </cite>
+      </div>
+    </div>
+    <div class="MainContentWrapper" id="App">
+      <div class="BoxWrapper">
+        <h1>Die Hypnochi App</h1>
+        <p class="Description">
+          Lade dir jetzt unsere App herunter.<br>Die App ist nur für Android im Play Store erhältlich.
+        </p>
+        <p id="AppLogoWrapper">
+          <a href="https://play.google.com/store/apps/dev?id=6735809974693992903">
+            <img src="./images/logo.png" alt="Logo" id="AppLogo">
+          </a>
+        </p>
+        <p id="AppSubtext">
+          <a href="https://play.google.com/store/apps/dev?id=6735809974693992903">Hypnochi</a> von
+          <a href="https://play.google.com/store/apps/dev?id=6735809974693992903">rommelbendel</a>&copy;
+        </p>
+      </div>
+
+      <div class="BoxWrapper" id="Handout">
+        <h1>Handout</h1>
+        <p class="Description">
+          Hier bekommst du die wichtigsten Informationen aus unserem Vortrag nocheinmal &uuml;bersichtlich und pr&auml;gnant zusammengefasst.
+        </p>
+        <p id="DownloadLinkWrapper">
+          <a href="./res/handout_hypnose.pdf" target="_blank" id="downloadLink" download>
+            herunterladen
+          </a>
+        </p>
+      </div>
+    </div>
+    <div class="Footer">
+      <p>
+        Diese Seite verwendet funktionale Cookies, die dabei helfen diese Seite funktionsf&auml;hig zu machen.
+      </p>
+      <p>
+        Copyright &copy; 2020 rommelbendel. Alle Rechte vorbehalten.
+      </p>
+    </div>
+  </body>
+</html>
